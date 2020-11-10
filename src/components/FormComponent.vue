ba865737b08a0743f205d64eca8df00933856f1a
Milan Peters <m.peters@graphicon.de>
2020-11-10 22:21:41 +0100

    change FormComponent.vue

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,6 +1,22 @@
 <template>
 	<section class="form-component">
-		<b-form-input v-model="title" placeholder="Enter title"></b-form-input>
+		<b-form @submit="onSubmit" @reset="onReset" v-if="show">
+			<b-form-input v-model="form.title" placeholder="Enter title..."></b-form-input>
+			<b-form-textarea
+					id="textarea"
+					v-model="form.text"
+					placeholder="Enter text..."
+					rows="3"
+					:maxlength="maxTextareaLength"
+			></b-form-textarea>
+			({{form.text.length}} / {{maxTextareaLength}})
+			<div>
+				<b-form-datepicker id="example-datepicker" v-model="form.date" ></b-form-datepicker>
+				<p>Value: '{{ form.date }}'</p>
+			</div>
+			<b-button type="submit" variant="primary">Submit</b-button>
+			<b-button type="reset" variant="danger">Reset</b-button>
+		</b-form>
 	</section>
 </template>
 
@@ -9,7 +25,41 @@
 		name: 'FormComponent',
 		data() {
 			return {
-				title: ''
+				form: {
+					title: '',
+					text: '',
+					date: ''
+				},
+				maxTextareaLength: 300,
+				show: true
+
+			}
+		},
+		methods: {
+			/**
+			 * submit form
+			 * @param evt
+			 */
+			onSubmit(evt) {
+				evt.preventDefault()
+				console.log(JSON.stringify(this.form))
+
+			},
+			/**
+			 * reset form
+			 * @param evt
+			 */
+			onReset(evt) {
+				evt.preventDefault()
+				// Reset our form values
+				this.form.title = ''
+				this.form.text = ''
+				this.form.date = ''
+				// Trick to reset/clear native browser form validation state
+				this.show = false
+				this.$nextTick(() => {
+					this.show = true
+				})
 			}
 		}
 	}
@@ -20,6 +70,14 @@
 	.form-component {
 		max-width: 500px;
 		margin:  0 auto;
+	}
+
+	.form-control {
+		margin: 20px 0 5px;
+	}
+
+	button {
+		margin: 0 20px;
 	}
 
 </style>
